631becc936b983966d0b938a4a6934a1e2d83894
JyotiAanaxa <jyoti@aanaxagorasr.com>
2022-02-21 21:54:27 +0530

    jojo

--- dev-manodayam-ott/src/pages/Videoplayer.js
+++ dev-manodayam-ott/src/pages/Videoplayer.js
@@ -24,33 +24,32 @@ export default function Videoplayer() {
   useEffect(() => {
     SleepApi();
   }, []);
-  
+
   return (
     <>
       <div>
         <div class="main-content side-content pt-0">
-          <div class="container-fluid">
-            <div class="inner-body">
-              <div class="col-lg-12 mt-3">
-                <div class="music-player-custom">
-                  <video
-                    id="my-video"
-                    class="video-js"
-                    controls
-                    preload="auto"
-                    poster="https://cdn.plyr.io/static/demo/View_From_A_Blue_Moon_Trailer-HD.jpg"
-                    data-setup=""
-                    loop
-                  >
-                    <source
-                      src="https://cdn.plyr.io/static/demo/View_From_A_Blue_Moon_Trailer-1080p.mp4"
-                      type="video/mp4"
-                    />
-                  </video>
+          {resData.map((element) => (
+            <div class="container-fluid">
+              <div class="inner-body">
+                <div class="col-lg-8">
+                  <div class="music-player-custom">
+                    <video
+                      id="my-video"
+                      class="video-js"
+                      controls
+                      preload="auto"
+                      poster={element.thumbnail_image}
+                      data-setup=""
+                      loop
+                    >
+                      <source src={element.video} type="video/mp4" />
+                    </video>
+                  </div>
                 </div>
               </div>
             </div>
-          </div>
+          ))}
         </div>
       </div>
     </>
