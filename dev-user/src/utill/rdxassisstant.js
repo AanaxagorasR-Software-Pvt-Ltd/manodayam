6c9969f06f2496ed9f8e53f858f39be8b465bec0
JyotiAanaxa <jyoti@aanaxagorasr.com>
2022-05-17 11:08:52 +0530

    jyoti

--- dev-user/src/utill/rdxassisstant.js
+++ dev-user/src/utill/rdxassisstant.js
@@ -1,7 +1,7 @@
 const voiceAssisstant = {
     link:
       window.location.hostname === "localhost"
-        ? "http://localhost:3000/"
+        ? "http://localhost:3020/"
         : "https://master.dmn3o3hk3w2r8.amplifyapp.com/",
   };
   export default voiceAssisstant;
